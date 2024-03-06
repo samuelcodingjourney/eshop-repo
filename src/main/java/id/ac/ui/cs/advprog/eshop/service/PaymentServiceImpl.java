c3153d452b650b4fcdbc8476bbd542562fdb0760
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-03-06 19:39:06 +0700

    [GREEN] Adding implementation for Payment Service

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -13,25 +13,44 @@ public class PaymentServiceImpl implements PaymentService {
     private OrderRepository orderRepository;
 
     public PaymentServiceImpl(PaymentRepository paymentRepository, OrderRepository orderRepository) {
+        this.paymentRepository = paymentRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        Payment payment = new Payment(UUID.randomUUID().toString(), method, "WAITING_PAYMENT", paymentData);
+        payment.setOrder(order);
+        paymentRepository.save(payment);
+        return payment;
     }
 
     @Override
     public void setStatus(Payment payment, String status) {
-
+        payment.setStatus(status);
+        if (status.equals("SUCCESS")) {
+            Order order = orderRepository.findById(payment.getOrder().getId());
+            if (order != null) {
+                order.setStatus("SUCCESS");
+                orderRepository.save(order);
+            }
+        } else if (status.equals("REJECTED")) {
+            Order order = orderRepository.findById(payment.getOrder().getId());
+            if (order != null) {
+                order.setStatus("FAILED");
+                orderRepository.save(order);
+            }
+        }
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-      
+        return paymentRepository.findAll();
     }
 }
 
