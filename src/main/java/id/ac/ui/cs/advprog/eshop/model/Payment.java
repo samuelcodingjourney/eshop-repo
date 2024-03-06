ec402e3279832f43d7a07015560b068f46fcfc7b
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-03-06 17:24:22 +0700

    [GREEN] Adding implementation of Payment Model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.eshop.model;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.Map;
 
@@ -15,16 +14,25 @@ public class Payment {
     private Map<String, String> paymentData;
 
     public Payment(String id, String method, String status, Map<String, String> paymentData) {
+        this.id = id;
+        this.method = method;
+        this.status = status;
+        this.paymentData = paymentData;
     }
 
     public void setStatus(String status) {
-
+        if (isValidStatus(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException("Invalid payment status: " + status);
+        }
     }
 
     private boolean isValidStatus(String status) {
-
+        return status.equals("SUCCESS") || status.equals("REJECTED");
     }
 }
+
 
 
 
